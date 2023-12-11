9f9abeea6a8adadc37293cf82a5aa64bd497e0a2
haoc.jin <jinhaocong@outlook.com>
2023-12-11 17:08:47 +0800

    fs

---
+++ study/2.Node.js & Express/代码/3.fs/13-批量重命名.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const path = require('path');
+
+const files = fs.readdirSync('./');
+
+const jsFiles = files.filter(file => path.extname(file) === '.js');
+
+console.log(jsFiles);
+
+jsFiles.forEach((file,index) => {
+    const data = file.split('-')
+    let [num,name] = data
+    num = (index + 1 + '').padStart(2, '0')
+    const newName = num + '-' + name
+    console.log(newName)
+
+    fs.renameSync(`./${file}`,`./${newName}`)
+})
