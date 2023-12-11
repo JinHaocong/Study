1bd6bbc8bf2133a4e95816e68da8d4e637044363
haoc.jin <jinhaocong@outlook.com>
2023-12-11 16:32:53 +0800

    fs

---
+++ study/2.Node.js & Express/代码/3.fs/11.两种路径.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+
+// sign 相对路径
+fs.writeFileSync('./index.html','love')
+fs.writeFileSync('../index.html','love')
+
+
+// sign 绝对路径
+fs.writeFileSync('C:\\Users\\jinha\\Desktop\\Study\\study\\2.Node.js & Express\\代码\\3.fs\\index.html','love')
