7afb956567affff822c909bfef42adf14c6bdba4
haoc.jin <jinhaocong@outlook.com>
2023-12-22 15:09:41 +0800

    jwt

--- study/2.Node.js & Express/code/12.会话控制/代码/03_token/jwt.js
+++ study/2.Node.js & Express/code/12.会话控制/代码/03_token/jwt.js
@@ -1,21 +1,24 @@
-//导入 jwt
+// 导入 jwt
 const jwt = require('jsonwebtoken');
 
-//创建(生成) token
-// let token = jwt.sign(用户数据, 加密字符串, 配置对象);
-// let token = jwt.sign({
-//   username: 'zhangsan'
-// }, 'atguigu', {
-//   expiresIn: 60, //单位是秒
-// })
+// 创建(生成) token
+const token = jwt.sign(
+  // 存储数据
+  {
+    username: 'admin',
+  },
+  // 加密字符串
+  'admin',
+  // 配置对象
+  {
+    expiresIn: 60, // 单位是秒
+  }
+);
 
-// console.log(token);
+console.log(token);
 
-let t =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InpoYW5nc2FuIiwiaWF0IjoxNjc3MzEzNTc1LCJleHAiOjE2NzczMTM2MzV9.fziAyCdYfhMYeM2a-XPMNZYdhIVYpluBoNR1c5oUm70';
-
-//校验 token
-jwt.verify(t, 'atguigu', (err, data) => {
+// 校验 token
+jwt.verify(token, 'admin', (err, data) => {
   if (err) {
     console.log('校验失败~~');
     return;
