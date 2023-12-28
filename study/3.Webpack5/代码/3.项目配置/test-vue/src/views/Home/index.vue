a6f8db1f21dc28c71de35acecf0b2f1010e71282
haoc.jin <jinhaocong@outlook.com>
2023-12-28 14:15:35 +0800

    webpack5 vue开发模式配置

---
+++ study/3.Webpack5/代码/3.项目配置/test-vue/src/views/Home/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1 class="home-title">Home~~~~~</h1>
+</template>
+
+<script>
+export default {
+  name: "HomeView",
+};
+</script>
+
+<style>
+.home-title {
+  color: #6214ff;
+  font-size: 30px;
+}
+</style>
