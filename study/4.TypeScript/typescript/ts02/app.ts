a76e7efea527c2162f138635d65bf3c42e0de040
haoc.jin <jinhaocong@outlook.com>
2024-01-02 16:57:52 +0800

    TypeScript tsconfig配置选项

--- study/4.TypeScript/typescript/ts02/app.ts
+++ study/4.TypeScript/typescript/ts02/app.ts
@@ -1,7 +1,11 @@
 // import {age} from './ex'
 
 console.log("啊哈哈");
-let a1:number;
-let b2:string;
+let a1: number;
+let b2: string;
+b2 = '555'
 // console.log(age);
 let age = 111;
+
+console.log(b2)
+
--- study/4.TypeScript/typescript/ts02/dict/app.js
+++ study/4.TypeScript/typescript/ts02/dict/app.js
@@ -2,7 +2,7 @@
 console.log("啊哈哈");
 let a1;
 let b2;
+b2 = '555';
 // console.log(age);
 let age = 111;
-// export const age = 22;
-const a = 11;
+console.log(b2);
