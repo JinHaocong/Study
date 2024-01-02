a87f523ee5c85ca2eed5f6667ba744d0f6c66699
haoc.jin <jinhaocong@outlook.com>
2024-01-02 12:00:40 +0800

    TypeScript

---
+++ study/4.TypeScript/.eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+    // 继承 Eslint 规则
+    extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    env: {
+        node: true, // 启用node中全局变量
+        browser: true, // 启用浏览器中全局变量
+    },
+    parserOptions: {
+        ecmaVersion: 2022, // es6
+        sourceType: "module", // es module
+    },
+    parser: "@typescript-eslint/parser",
+    plugins: ["@typescript-eslint"],
+    rules: {
+        "no-var": 2,
+        "no-unused-vars": 0,
+        "prefer-const": 0,
+        "no-inner-declarations": 0,
+        "@typescript-eslint/no-unused-vars": 0
+    },
+};
---
+++ study/4.TypeScript/typescript/ts01/02-basis.ts
@@ -0,0 +1,28 @@
+{
+    // 声明一个变量a，类型为number
+    let a: number;
+
+    // a 的类型设置为number，在以后的过程中a的值只能为数字
+    a = 100;
+    // a = 'a';
+
+    let b: string;
+    b = 'aaa';
+
+    // 声明完直接赋值
+    const c: boolean = true;
+
+    // 不声明类型会默认为第一次赋值的变量的类型为自己的类型
+    const d = '111';
+    // d = 222;
+
+    // 返回值类型也是必须为number
+    function sum(x: number, y: number): number {
+        console.log(x + y)
+        return x + y;
+    }
+
+    sum(1, 2);
+    // sum(1,'2');
+
+}
