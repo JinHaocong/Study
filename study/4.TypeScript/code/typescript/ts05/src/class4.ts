2cb8af9c9ab452bd351df12f4b787a857d874c72
haoc.jin <jinhaocong@outlook.com>
2024-01-03 15:49:16 +0800

    webpack 面向对象 继承 super关键字

--- study/4.TypeScript/code/typescript/ts05/src/class4.ts
+++ study/4.TypeScript/code/typescript/ts05/src/class4.ts
@@ -1,27 +1,63 @@
 // super关键字
+/**
+ * Obj类
+ * @class Obj
+ * @property {string} name
+ * @constructor
+ */
 class Obj {
     name: string;
 
+    /**
+     * @param {string} name
+     */
     constructor(name: string) {
         this.name = name;
     }
 
+    /**
+     * sayHello
+     * @function sayHello
+     */
     sayHello() {
         console.log("哈哈哈")
     }
 }
 
+/**
+ * Stu类
+ * @class Stu
+ * @property {number} age
+ * @extends Obj
+ * @constructor
+ * @param {string} name
+ * @param {number} age
+ */
 class Stu extends Obj {
     age: number;
 
+    /**
+     * Creates an instance of Stu.
+     * @param {string} name
+     * @param {number} age
+     */
     constructor(name: string, age: number) {
         // 调用父类的构造方法
         super(name);
         this.age = age;
     }
 
+    /**
+     * 调用父类的 sayHello
+     * @function sayHello
+     */
     sayHello() {
         // super 表示直接父类，在类的方法中
         super.sayHello();
     }
 }
+
+const stu = new Stu('jhc', 23);
+
+console.log(stu, 'stu')
+stu.sayHello()
