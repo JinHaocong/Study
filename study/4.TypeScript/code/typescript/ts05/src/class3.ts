dea94e8693998479847bc5d2b28021b2b405a4bb
haoc.jin <jinhaocong@outlook.com>
2024-01-03 11:33:51 +0800

    webpack 面向对象 继承

--- study/4.TypeScript/code/typescript/ts05/src/class3.ts
+++ study/4.TypeScript/code/typescript/ts05/src/class3.ts
@@ -31,27 +31,56 @@
 // }
 
 // 定一个动物类
+/**
+ * @class Animal
+ * @param {string} name - The name of the animal.
+ * @param {number} age - The age of the animal.
+ */
 class Animal {
-    name: string;
+
+    name: string
     age: number;
 
+    // 私有属性在子类中无法直接访问
+    private test: string = '123123'
+
+    /**
+     * Creates a new Animal instance.
+     * @param {string} name - The name of the animal.
+     * @param {number} age - The age of the animal.
+     */
     constructor(name: string, age: number) {
         this.name = name;
         this.age = age;
     }
 
+    /**
+     * Prints "Animal says hello" to the console.
+     */
     sayHello() {
-        console.log("动物叫！")
+        console.log("Animal says hello");
+    }
+
+    run() {
+        console.log(this.name + '在跑~~~~~')
+    }
+
+    logTest(): void {
+        console.log(this.test);
     }
 }
 
-// 定义dog类继承animal
+/**
+ * @class Dog
+ * @extends Animal
+ */
 class Dog extends Animal {
     /*
-    * 此时，animal被称为父类，dog被称为子类
-    * 使用继承后，子类将会用于父类所有的方法和属性（方法和属性都是静态和实例属性）,方法和属性不能是私有的
-    * 子类是可以添加父类没有的方法和属性
-    * */
+        * 此时，animal被称为父类，dog被称为子类
+        * 使用继承后，子类将会用于父类所有的方法和属性（方法和属性都是静态和实例属性）,方法和属性不能是私有的
+        * 子类是可以添加父类没有的方法和属性
+        * 如果子类添加了和父类同名的方法，就会覆盖掉父类的方法，叫做方法的重写
+    */
 
     // 方法重写
     sayHello() {
@@ -59,25 +88,36 @@ class Dog extends Animal {
     }
 }
 
-// 定义cat继承animal
+/**
+ * @class Cat
+ * @extends Animal
+ */
 class Cat extends Animal {
 
     sayHello() {
         console.log('喵喵喵！')
     }
+
+    logThis(): void {
+        console.log(this, 'Cat this')
+    }
 }
 
 
 const dog = new Dog('旺财', 22);
-console.log(dog);
+console.log(dog, 'dog');
 dog.sayHello();
+dog.run()
 
 const cat = new Cat('咪咪', 11);
-console.log(cat);
+console.log(cat, 'cat');
 cat.sayHello()
+cat.logThis()
+cat.run()
 
 // 多态
 let animal = new Dog('小狗狗', 22);
 console.log('--------------------');
 console.log(animal);
 animal.sayHello()
+animal.run()
