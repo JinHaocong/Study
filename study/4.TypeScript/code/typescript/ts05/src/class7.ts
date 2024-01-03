f1a3d77926868dbe6336f041e244f83dd2ba23f2
haoc.jin <jinhaocong@outlook.com>
2024-01-03 17:21:15 +0800

    webpack 面向对象 属性的封装 private public get set

--- study/4.TypeScript/code/typescript/ts05/src/class7.ts
+++ study/4.TypeScript/code/typescript/ts05/src/class7.ts
@@ -1,21 +1,35 @@
 // 属性的封装
+/**
+ * @class User
+ * @classdesc 封装属性
+ * @param {string} name 用户名
+ * @param {number} age 年龄
+ */
 class User {
+    public gender: string
+
     // 属性都改为私有的，外界无法直接访问
-    /*
-    *
-    * 属性应该是不能让外界随意修改，那样数据不安全
-    * 在属性前加上属性修饰符，不让用户修改属性
-    * private 私有属性 有该属性修饰符，只能在类的内部才能进行修改
-    * 可以在类中添加方法可以被外部访问 get方法获取，set 方法设置
-    * */
-    private _name: string;
-    private _age: number;
-
-
-    constructor(name: string, age: number) {
+    /**
+     * @constructor
+     * @param {string} name 用户名
+     * @param {number} age 年龄
+     * @param {gender} gender 性别
+     */
+    constructor(name: string, age: number, gender: string) {
         this._name = name;
         this._age = age;
+        this.gender = gender;
     }
+
+    private _name: string;
+
+    /*
+        * 属性应该是不能让外界随意修改，那样数据不安全
+        * 在属性前加上属性修饰符，不让用户修改属性
+        * private 私有属性 有该属性修饰符，只能在当前类的内部才能进行修改
+        * public 属性可以在任意地方访问，修改
+        * 可以在类中添加方法可以被外部访问 get方法获取，set 方法设置
+    */
 
     get name(): string {
         return this._name;
@@ -25,37 +39,86 @@ class User {
         this._name = value;
     }
 
+    private _age: number;
+
     get age(): number {
         return this._age;
     }
 
+    /**
+     * Sets the age of the user.
+     * @param {number} value The new age of the user.
+     * @throws {Error} If the age is less than or equal to zero.
+     */
     set age(value: number) {
         if (value > 0) {
             this._age = value;
         } else {
-            throw new Error('年龄不能小于0')
+            throw new Error('Age cannot be less than or equal to zero');
         }
     }
 }
 
-const user = new User('aa', 11);
-// user.name = '1';
-console.log(user.name)
-console.log(user)
+const user = new User('aa', 11, '男');
+console.log(user, 'user1');
+user.name = '1';
+console.log(user, 'user2')
 user.age = 1;
+console.log(user, 'user3')
+
+// 抛出错误
+try {
+    user.age = -69
+} catch (err) {
+    console.log(err);
+}
 
 // 可以直接将属性的声明放到构造函数中，但是需要加上权限修饰符
+/**
+ * @class D
+ * @classdesc A class with a private constructor
+ * @param {string} name The name of the object
+ */
 class D {
-    // 其实就是语法糖的写法
-    constructor(public name: string, public age: number) {
-        // this.name = name; // 已经不需要写了
+
+    /**
+     * Creates a new instance of D
+     * @param {string} name The name of the object
+     * @param {age} age The age of the object
+     * @param {sex} sex The sex of the object
+     */
+    constructor(public name: string, public age: number, protected sex: string) {
+        console.log(this, 'this')
     }
 }
 
+/**
+ * @class E
+ * @classdesc A class with a private constructor
+ * @param {string} name The name of the object
+ */
 class E {
+    /**
+     * Creates a new instance of E
+     * @param {string} name The name of the object
+     */
     constructor(private name: string) {
+        console.log(this.name)
     }
 }
 
-console.log(new D('1', 2));
+class H extends D {
+    test() {
+        // 不可访问
+        // console.log(this.name)
+
+        // protected 在类中可以访问 外面不可访问
+        console.log(this.sex)
+    }
+}
+
+const fff = new H('jhc', 23, '男')
+fff.test()
+
+console.log(new D('1', 2, '男'));
 console.log(new E('222'))
