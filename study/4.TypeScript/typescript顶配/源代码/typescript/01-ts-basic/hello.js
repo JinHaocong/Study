a6c7790679376dfe1a8e8212272f800a5d9bbae1
haoc.jin <jinhaocong@outlook.com>
2024-01-05 17:55:09 +0800

    typescript顶配笔记

--- study/4.TypeScript/typescript顶配/源代码/typescript/01-ts-basic/hello.js
+++ study/4.TypeScript/typescript顶配/源代码/typescript/01-ts-basic/hello.js
@@ -1,12 +1,14 @@
-"use strict";
 // // 你好，世界
 // console.log('Hello World')
 function greet(person, date) {
-    console.log(`Hello ${person}, today is ${date}.`);
+    console.log("Hello ".concat(person, ", today is ").concat(date, "."));
 }
 greet('小锋', new Date());
-// let msg = 'hello there!'
-// msg = 'hello world'
-// msg = 100
-let surname = undefined;
-let age = null;
+var msg = 'hello there!';
+msg = 'hello world';
+console.log(msg, 'msg');
+// msg = 100  报错
+// let surname: string = undefined
+// let age: number = null
+//
+// console.log(surname, age)
