f5caf755d8567fa1ce67693f45931301e0daba06
haoc.jin <jinhaocong@outlook.com>
2024-01-05 17:27:58 +0800

    typescript顶配笔记

--- study/4.TypeScript/typescript顶配/源代码/typescript/01-ts-basic/hello.ts
+++ study/4.TypeScript/typescript顶配/源代码/typescript/01-ts-basic/hello.ts
@@ -1,15 +1,18 @@
 // // 你好，世界
 // console.log('Hello World')
 
-function greet(person, date) {
-  console.log(`Hello ${person}, today is ${date}.`)
+function greet(person: string, date: Date) {
+    console.log(`Hello ${person}, today is ${date}.`)
 }
 
 greet('小锋', new Date())
 
-// let msg = 'hello there!'
-// msg = 'hello world'
-// msg = 100
+let msg = 'hello there!'
+msg = 'hello world'
+console.log(msg, 'msg')
+// msg = 100  报错
 
-let surname: string = undefined
-let age: number = null
+// let surname: string = undefined
+// let age: number = null
+//
+// console.log(surname, age)
