881d28e774d248f756f005cf8b20daabfcbc6c0f
haoc.jin <jinhaocong@outlook.com>
2024-01-22 11:18:07 +0800

    项目移除

--- study/6.项目/viteAdmin/src/api/modules/richText.ts
+++
@@ -1,8 +0,0 @@
-import request from "@/utils/request";
-
-// 查询富文本
-export function richTextFun() {
-  return request({
-    url: "modules/richText",
-  });
-}
--- study/6.项目/viteAdmin/src/views/login/locales/zh-TW.ts
+++
@@ -1,5 +0,0 @@
-export default {
-  'login': '登錄',
-  'please.enter.a.account.number': '請輸入賬號',
-  'please.enter.a.PIN': '請輸入密碼',
-}
