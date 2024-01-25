6e93b3f5b2d18c04bf1b14452f2dd63fc8df3433
‘jhc’ <jinhaocong@outlook.com>
2024-01-25 12:06:45 +0800

    vue3:组件通信 v-model

--- study/6.Vue3/create-vue/src/pages/04_v-model/Father.vue
+++ study/6.Vue3/create-vue/src/pages/04_v-model/Father.vue
@@ -3,28 +3,31 @@
     <h3>父组件</h3>
     <h4>{{ username }}</h4>
     <h4>{{ password }}</h4>
-    <!-- v-model用在html标签上 -->
-    <!-- <input type="text" v-model="username"> -->
-    <!-- <input type="text" :value="username" @input="username = (<HTMLInputElement>$event.target).value"> -->
+    <!-- sign v-model用在html标签上 -->
+    <!--    <input v-model="username" type="text" />-->
+    <!--    <input :value="username" type="text" @input="handleInput($event)" />-->
 
-    <!-- v-model用在组件标签上 -->
-    <!-- <AtguiguInput v-model="username"/> -->
-    <!-- <AtguiguInput 
-      :modelValue="username" 
-      @update:modelValue="username = $event"
-    /> -->
+    <!--sign v-model用在组件标签上 -->
+    <!--sign v-model的底层就是 :modelValue="username" @update:modelValue="setModelValue" -->
+    <!--    <Input v-model="username" />-->
+    <Input :modelValue="username" @update:modelValue="setModelValue" />
 
-    <!-- 修改modelValue -->
-    <AtguiguInput v-model:ming="username" v-model:mima="password"/>
+    <!-- sign  修改 modelValue 别名-->
+    <Input v-model:name="username" v-model:pwd="password" />
   </div>
 </template>
 
-<script setup lang="ts" name="Father">
-	import { ref } from "vue";
-  import AtguiguInput from './AtguiguInput.vue'
-  // 数据
-  let username = ref('zhansgan')
-  let password = ref('123456')
+<script lang="ts" name="Father" setup>
+import { ref } from 'vue'
+import Input from './Input.vue'
+
+// 数据
+let username = ref('jhc')
+let password = ref('123456')
+
+function setModelValue(value: string) {
+  username.value = value
+}
 </script>
 
 <style scoped>
---
+++ study/6.Vue3/create-vue/src/pages/04_v-model/Input.vue
@@ -0,0 +1,38 @@
+<template>
+  <input v-if="name" :value="name" type="text" @input="updateName($event)" />
+  <br />
+  <input v-if="pwd" :value="pwd" type="text" @input="updatePwd($event)" />
+
+  <input v-if="modelValue" :value="modelValue" type="text" @input="updateModelValue($event)" />
+</template>
+
+<script lang="ts" name="Input" setup>
+// sign props
+defineProps(['name', 'pwd', 'modelValue'])
+
+// sign emit 事件
+const emit = defineEmits(['update:name', 'update:pwd', 'update:modelValue'])
+
+// sign input 事件
+function updatePwd(event: Event) {
+  emit('update:pwd', (event.target as HTMLInputElement).value)
+}
+
+function updateName(event: Event) {
+  emit('update:name', (event.target as HTMLInputElement).value)
+}
+
+function updateModelValue(event: Event) {
+  emit('update:modelValue', (event.target as HTMLInputElement).value)
+}
+</script>
+
+<style scoped>
+input {
+  border: 2px solid black;
+  background-image: linear-gradient(45deg, red, yellow, green);
+  height: 30px;
+  font-size: 20px;
+  color: white;
+}
+</style>
