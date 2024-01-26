506c09fcc14a930d2965dddfc08703a0cb07361f
‘jhc’ <jinhaocong@outlook.com>
2024-01-26 17:17:24 +0800

    fix(vue3-ante-plus):日志中间件报错，request换成axios请求，逻辑重构。

--- study/10.项目/vue3-antd-plus/SERVER/middlewares/actionLogMiddleware.js
+++ study/10.项目/vue3-antd-plus/SERVER/middlewares/actionLogMiddleware.js
@@ -1,70 +1,81 @@
 const chalk = require('chalk');
 const UAParser = require("ua-parser-js");
-const request = require("request");
 const {UsersOptLogModel} = require('@models/v1');
+const isDev = process.env.NODE_ENV === 'development';
+const axios = require('axios');
+const os = require('os');
 
-// IP地址解析
-function parseIP(clientIp) {
-    return new Promise((resolve, reject) => {
-        request(
-            `https://restapi.amap.com/v3/ip?ip=[${clientIp}]&output=xml&key=ea3dd8eac84988e976da4f3027e6b3d8`,
-            {method: 'GET'},
-            function (error, response, body) {
-                if (error !== null) {
-                    resolve('API:地址获取失败')
-                    return
-                }
-                if (body && !body.status) {
-                    resolve(body.length && JSON.parse(body).data[0].location || '-');
-                }
-            }
-        );
+function getLocalIPv4Address() {
+    const networkInterfaces = os.networkInterfaces();
+    let ipv4Address = '127.0.0.1';
+
+    Object.keys(networkInterfaces).some((interfaceName) => {
+        const interfaceInfo = networkInterfaces[interfaceName];
+        const info = interfaceInfo.find((info) => info.family === 'IPv4' && !info.internal);
+
+        if (info) {
+            ipv4Address = info.address;
+            return true; // break the loop
+        }
+
+        return false;
     });
+
+    return ipv4Address;
 }
 
-// 获取用户的真实公网IP
+async function parseIP(clientIp) {
+    try {
+        const response = await axios.get(`https://restapi.amap.com/v3/ip?ip=[${clientIp}]&output=json&key=${process.env.API_MAP_KEY}`);
+        const {status, province, city} = response.data;
+
+        if (status === '1') {
+            const resolvedProvince = province.length ? province : '';
+            const resolvedCity = city.length ? city : '';
+            return resolvedProvince + resolvedCity;
+        } else {
+            return 'API:地址获取失败';
+        }
+    } catch (err) {
+        return 'API:地址获取失败';
+    }
+}
+
 function getPublicIP(req) {
     const headers = req.headers;
+
     if (headers['x-real-ip']) {
         return headers['x-real-ip'];
-    }
-    if (headers['x-forwarded-for']) {
+    } else if (headers['x-forwarded-for']) {
         const ipList = headers['x-forwarded-for'].split(',');
         return ipList[0];
     }
+
     return '0.0.0.0';
 }
 
-/**
- * 记录操作日志.
- * @param {string}  module 模块名
- * @param {string}  content 操作内容
- * @returns {Function}
- */
-const actionRecords = ({module, content}) => {
-    return async (req, res, next) => {
-        try {
-            const clientIP = getPublicIP(req);
-            //识别常见的浏览器、操作系统和设备等信息
-            const u = new UAParser(req.headers['user-agent']);
-            const address = await parseIP(clientIP);
+const actionRecords = ({module, content}) => async (req, res, next) => {
+    try {
+        const clientIP = isDev ? getLocalIPv4Address() : getPublicIP(req);
+        const u = new UAParser(req.headers['user-agent']);
+        const address = await parseIP(clientIP);
 
-            const newUsersOptLog = {
-                operatorId: req.userId || '-',
-                operator: req.user?.nickname || req.body.username || '未知',
-                module,
-                platform: u.getBrowser().name ? `${u.getBrowser().name}.v${u.getBrowser().major}` : '未知',
-                operatorIP: clientIP,
-                address: address || '-',
-                content: content || `${req.originalUrl}`
-            };
+        const newUsersOptLog = {
+            operatorId: req.userId || '未知',
+            operator: req.user?.nickname || req.body.username || '未知',
+            module,
+            platform: u.getBrowser().name ? `${u.getBrowser().name}.v${u.getBrowser().major}` : '未知',
+            operatorIP: clientIP,
+            address: address || '未知',
+            content: content || req.originalUrl
+        };
 
-            await UsersOptLogModel.create(newUsersOptLog);
-            next();
-        } catch (err) {
-            console.error(chalk.red('操作日志记录失败'), err);
-        }
-    };
+        await UsersOptLogModel.create(newUsersOptLog);
+        next();
+    } catch (err) {
+        console.error(chalk.red('操作日志记录失败'), err);
+        next(err);
+    }
 };
 
 module.exports = {actionRecords};
