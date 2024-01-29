9dbb7a6ca04d8af90838e8168eb24c7e7cfadb8c
‘jhc’ <jinhaocong@outlook.com>
2024-01-29 17:48:02 +0800

    后端项目初始化 登录注册接口完成

--- study/10.项目/vue3-ts-express/server/handler/login.js
+++ study/10.项目/vue3-ts-express/server/handler/login.js
@@ -7,17 +7,13 @@ exports.register = async (req, res) => {
   try {
     // step 1：检查账号或密码是否为空
     const { account, password } = req.body;
-    if (!account || !password) {
-      return res.error('账号或者密码不能为空');
-    }
+    if (!account || !password) return res.error('账号或者密码不能为空');
 
     // step 2：检查账号是否已经存在
     const querySQL = 'SELECT * FROM users WHERE account = ?';
     const [queryData] = await db.query(querySQL, account) || [];
 
-    if (queryData.length) {
-      return res.error('账号已存在');
-    }
+    if (queryData.length) return res.error('账号已存在');
 
     // step 3：加密密码
     const encryptedPassword = bcrypt.hashSync(password, 10);
@@ -35,9 +31,7 @@ exports.register = async (req, res) => {
     const [insertData] = await db.query(insertSQL, userInfo);
 
     // 确保之影响一行数据
-    if (insertData.affectedRows !== 1) {
-      return res.error('注册账号失败');
-    }
+    if (insertData.affectedRows !== 1) return res.error('注册账号失败');
 
     res.success('注册账号成功');
   } catch (e) {
@@ -52,20 +46,14 @@ exports.login = async (req, res) => {
     // step 1：查看数据表中有没有前端传过来的账号
     const querySQL = 'select * from users where account = ?';
     const [queryData] = await db.query(querySQL, account) || [];
-    if (!queryData.length) {
-      return res.error('登录失败：用户不存在');
-    }
+    if (!queryData.length) return res.error('登录失败：用户不存在');
 
     // step 2：对前端传过来的密码进行解密
     const compareResult = bcrypt.compareSync(password, queryData[0].password);
-    if (!compareResult) {
-      return res.error('登录失败：密码错误');
-    }
+    if (!compareResult) return res.error('登录失败：密码错误');
 
     // step 3：对账号是否冻结做判定
-    if (queryData[0].status === '1') {
-      return res.error('登录失败：账号被冻结');
-    }
+    if (queryData[0].status === '1') return res.error('登录失败：账号被冻结');
 
     // step 4：生成返回给前端的token 剔除加密后的密码,头像,创建时间,更新时间
     const user = {
