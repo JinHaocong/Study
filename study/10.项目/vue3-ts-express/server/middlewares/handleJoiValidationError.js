be9b7af3bc15cd05da25c9e2fb8fba55a79f00be
‘jhc’ <jinhaocong@outlook.com>
2024-01-30 15:51:28 +0800

    后端项目增加joi校验

--- study/10.项目/vue3-ts-express/server/middlewares/handleJoiValidationError.js
+++ study/10.项目/vue3-ts-express/server/middlewares/handleJoiValidationError.js
@@ -1,7 +1,7 @@
 const Joi = require('joi');
 
 const handleJoiValidationError = (err, req, res, next) => {
-  if (err instanceof Joi.ValidationError) return res.error('输入的数据不符合验证规则');
+  if (err instanceof Joi.ValidationError) return res.error(err.message);
   next(err);
 };
 
