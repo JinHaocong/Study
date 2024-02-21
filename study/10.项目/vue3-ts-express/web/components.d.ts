c565a88bfa90c5cf632f3814e1e4fc13dbb98a5f
‘jhc’ <jinhaocong@outlook.com>
2024-02-21 17:05:51 +0800

    系统设置账号详情

--- study/10.项目/vue3-ts-express/web/components.d.ts
+++ study/10.项目/vue3-ts-express/web/components.d.ts
@@ -31,18 +31,15 @@ declare module 'vue' {
     ElMenu: typeof import('element-plus/es')['ElMenu']
     ElMenuItem: typeof import('element-plus/es')['ElMenuItem']
     ElMenuItemGroup: typeof import('element-plus/es')['ElMenuItemGroup']
-    ElOption: typeof import('element-plus/es')['ElOption']
-    ElSelect: typeof import('element-plus/es')['ElSelect']
+    ElOption: (typeof import('element-plus/es'))['ElOption']
+    ElSelect: (typeof import('element-plus/es'))['ElSelect']
     ElSubMenu: typeof import('element-plus/es')['ElSubMenu']
     ElTabPane: typeof import('element-plus/es')['ElTabPane']
     ElTabs: typeof import('element-plus/es')['ElTabs']
-    ElTag: typeof import('element-plus/es')['ElTag']
-    ElUpload: typeof import('element-plus/es')['ElUpload']
+    ElTag: (typeof import('element-plus/es'))['ElTag']
+    ElUpload: (typeof import('element-plus/es'))['ElUpload']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
     SvgIcon: typeof import('./src/components/SvgIcon.vue')['default']
-  }
-  export interface ComponentCustomProperties {
-    vLoading: typeof import('element-plus/es')['ElLoadingDirective']
   }
 }
