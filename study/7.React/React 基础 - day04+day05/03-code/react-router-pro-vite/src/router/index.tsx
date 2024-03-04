6a6965c636154158309b2423d70899c41c87805a
‘jhc’ <jinhaocong@outlook.com>
2024-03-04 17:34:52 +0800

    ReactRouter  嵌套路由配置

--- study/7.React/React 基础 - day04+day05/03-code/react-router-pro-vite/src/router/index.tsx
+++ study/7.React/React 基础 - day04+day05/03-code/react-router-pro-vite/src/router/index.tsx
@@ -1,11 +1,25 @@
 import {createBrowserRouter} from "react-router-dom";
 import Login from "../page/login";
 import Article from "../page/Article";
+import Layout from "../page/Layout";
+import Board from "../page/Board";
+import About from "../page/About";
 
 const router = createBrowserRouter([
     {
         path: '/',
-        element: <Login/>
+        element: <Layout/>,
+        children: [
+            {
+                // 设置为默认二级路由 一级路由访问的时候，它也能得到渲染
+                index: true,
+                element: <Board/>
+            },
+            {
+                path: 'about',
+                element: <About/>
+            }
+        ]
     },
     {
         path: '/login',
