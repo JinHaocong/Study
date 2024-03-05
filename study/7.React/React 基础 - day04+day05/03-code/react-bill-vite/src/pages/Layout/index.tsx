a6e14854cf5d7d1110350dec08467310f82e837a
‘jhc’ <jinhaocong@outlook.com>
2024-03-05 11:35:21 +0800

    feat:antd-mobile主题色定制

--- study/7.React/React 基础 - day04+day05/03-code/react-bill-vite/src/pages/Layout/index.tsx
+++ study/7.React/React 基础 - day04+day05/03-code/react-bill-vite/src/pages/Layout/index.tsx
@@ -1,9 +1,17 @@
 import {Outlet} from "react-router-dom";
+import {Button} from "antd-mobile";
 
 const Layout = () => {
     return (
         <div>
             Layout
+            {/*全局样式测试*/}
+            <Button color={"primary"}>测试</Button>
+
+            {/*局部样式测试*/}
+            <div className={'purple'}>
+                <Button color={"primary"}>测试</Button>
+            </div>
             <Outlet/>
         </div>
     );
