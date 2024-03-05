70f906860036d07312ebd590cc310dd6cc1080bf
‘jhc’ <jinhaocong@outlook.com>
2024-03-05 12:13:59 +0800

    feat:添加首屏loading动画

--- study/7.React/React 基础 - day04+day05/03-code/react-bill-vite/src/main.tsx
+++ study/7.React/React 基础 - day04+day05/03-code/react-bill-vite/src/main.tsx
@@ -4,7 +4,7 @@ import App from './App.tsx'
 import './main.scss'
 import './theme.scss'
 
-ReactDOM.createRoot(document.getElementById('root1')!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
     // <React.StrictMode>
     <App/>
     // </React.StrictMode>,
