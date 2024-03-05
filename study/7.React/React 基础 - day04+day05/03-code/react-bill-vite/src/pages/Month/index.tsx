19530cfa6d1c91e511a6fe944cf87b415a4d2024
‘jhc’ <jinhaocong@outlook.com>
2024-03-05 16:29:57 +0800

    feat:月度账单-统计区域-切换弹框

--- study/7.React/React 基础 - day04+day05/03-code/react-bill-vite/src/pages/Month/index.tsx
+++ study/7.React/React 基础 - day04+day05/03-code/react-bill-vite/src/pages/Month/index.tsx
@@ -1,9 +1,60 @@
-const Month = () => {
-    return (
-        <div>
-            Month
-        </div>
-    );
-};
+import {NavBar, DatePicker} from 'antd-mobile'
+import './index.scss'
+import {useState} from "react";
+import classNames from "classnames";
 
-export default Month;
+const Month = () => {
+    const [dateVisible, setDateVisible] = useState(false)
+    // 时间选择框确实事件
+    const dateConfirm = (date: Date) => {
+        console.dir(date)
+        // 关闭弹框
+        setDateVisible(false)
+    }
+    return (
+        <div className="monthlyBill">
+            <NavBar className="nav" backArrow={false}>
+                月度收支
+            </NavBar>
+            <div className="content">
+                <div className="header">
+                    {/* 时间切换区域 */}
+                    <div className="date" onClick={() => setDateVisible(true)}>
+            <span className="text">
+              2023 | 3月账单
+            </span>
+                        <span className={classNames('arrow', {expand: dateVisible})}></span>
+                    </div>
+                    {/* 统计区域 */}
+                    <div className='twoLineOverview'>
+                        <div className="item">
+                            <span className="money">{100}</span>
+                            <span className="type">支出</span>
+                        </div>
+                        <div className="item">
+                            <span className="money">{200}</span>
+                            <span className="type">收入</span>
+                        </div>
+                        <div className="item">
+                            <span className="money">{200}</span>
+                            <span className="type">结余</span>
+                        </div>
+                    </div>
+                    {/* 时间选择器 */}
+                    <DatePicker
+                        className="kaDate"
+                        title="记账日期"
+                        precision="month"
+                        visible={dateVisible}
+                        max={new Date()}
+                        onConfirm={dateConfirm}
+                        onClose={() => setDateVisible(false)}
+                        onCancel={() => setDateVisible(false)}
+                    />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Month
