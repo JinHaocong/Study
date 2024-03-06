e2556a6544c284d1f617b29fa9db31c716db9cf7
‘jhc’ <jinhaocong@outlook.com>
2024-03-06 16:14:12 +0800

    feat:Antd添加主题配置

--- study/10.项目/react-ts-vite-jike/src/App.tsx
+++ study/10.项目/react-ts-vite-jike/src/App.tsx
@@ -1,12 +1,24 @@
 import {RouterProvider} from "react-router-dom";
 import router from "./router";
+import {ConfigProvider} from "antd";
 
 function App() {
 
 
     return (
         <>
-            <RouterProvider router={router}></RouterProvider>
+            <ConfigProvider theme={{
+                token: {
+                    // Seed Token，影响范围大
+                    colorPrimary: '#69AE78FF',
+                    borderRadius: 4,
+
+                    // 派生变量，影响范围小
+                    colorBgContainer: '#f6ffed',
+                },
+            }}>
+                <RouterProvider router={router}></RouterProvider>
+            </ConfigProvider>
         </>
     )
 }
