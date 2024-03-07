1bbb315da1e4a1bc94a86222e00e2a0c59ad6aab
‘jhc’ <jinhaocong@outlook.com>
2024-03-07 14:22:41 +0800

    feat:封装hook useToken

---
+++ study/10.项目/react-ts-vite-jike/src/hooks/useToken.ts
@@ -0,0 +1,23 @@
+import {useAppDispatch, useAppSelector} from "@/hooks/storeHooks.ts";
+import {useCallback} from "react";
+import {setUserToken} from "@/store/module/user/userSlice.ts";
+
+const useToken = () => {
+    const dispatch = useAppDispatch()
+    const token = useAppSelector(state => state.user.token)
+
+    const setToken = useCallback((token: string) => {
+        dispatch(setUserToken(token))
+    }, [dispatch])
+
+    const clearToken = useCallback(() => {
+        setToken('')
+    }, [setToken])
+    return {
+        token,
+        setToken,
+        clearToken
+    }
+}
+
+export default useToken
