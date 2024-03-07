48f768507efa39d06d2a6b98058a97887a5f6717
‘jhc’ <jinhaocong@outlook.com>
2024-03-07 12:17:47 +0800

    feat:封装util storage

--- study/10.项目/react-ts-vite-jike/src/util/index.ts
+++ study/10.项目/react-ts-vite-jike/src/util/index.ts
@@ -1,6 +1,24 @@
 // 统一中转导出
 import instance from './request.ts'
+import {
+    getItem,
+    getItems,
+    getSessionItem,
+    getSessionItems,
+    setItem,
+    setItems,
+    setSessionItem,
+    setSessionItems
+} from './storage.ts'
 
 export {
-    instance
+    instance,
+    setItem,
+    setItems,
+    getItem,
+    getItems,
+    setSessionItem,
+    setSessionItems,
+    getSessionItem,
+    getSessionItems
 }
