6c9f319a6b8cf0a255563d7df80384ac5d0b7c69
‘jhc’ <jinhaocong@outlook.com>
2024-03-12 11:37:15 +0800

    feat:配置Vite打包可视化

--- study/10.项目/react-ts-vite-jike/vite.config.ts
+++ study/10.项目/react-ts-vite-jike/vite.config.ts
@@ -2,6 +2,7 @@ import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from "path";
 import viteCompression from 'vite-plugin-compression';
+import {visualizer} from 'rollup-plugin-visualizer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,6 +12,7 @@ export default defineConfig({
             // 压缩后删除源文件
             deleteOriginFile: true
         }),
+        visualizer({open: true})
     ],
     resolve: {
         alias: {
